7bee7d6a0da62c438e20d555f6f3cbe100db74b3
Dacey Nolan <daceyshirley@gmail.com>
2020-03-18 18:07:10 -0400

    Configure header for authorization

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,13 @@
 import React from "react";
 
+import http from "../../utils/http";
+import useAsyncState from "../../hooks/useAsyncState";
+
+const fetchShotAverages = () => http.get("/shots/aggregate");
+
 const Dashboard = () => {
+  const [shotAverages] = useAsyncState(fetchShotAverages);
+  console.log(shotAverages);
   return <div>Dashboard</div>;
 };
 
--- src/utils/http.js
+++ src/utils/http.js
@@ -2,4 +2,16 @@ import axios from "axios";
 
 const http = axios.create({ baseURL: "http://localhost:3090" });
 
+http.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
 export default http;
