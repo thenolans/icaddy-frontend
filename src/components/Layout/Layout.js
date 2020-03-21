9804259b677c8dcaa9689896798938421e5a9850
Dacey Nolan <daceyshirley@gmail.com>
2020-03-20 21:28:56 -0400

    Create nav and clear token on logout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import "./layout.scss";
 import Container from "../Container";
+import Nav from "../Nav";
 
 const Layout = props => {
   return (
     <div className="layout">
       <div className="layout__top-banner">
-        <div className="layout__nav">Nav bar</div>
+        <Container className="layout__nav pt-5">
+          <Nav />
+        </Container>
       </div>
       <Container className="layout__content p-3 mb-3">
         {props.children}
